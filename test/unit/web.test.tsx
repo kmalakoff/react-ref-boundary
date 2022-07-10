14b0cf8d8fa7f0a2ed1f168c5cd14c689a14fc09
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-10 02:03:49 -0700

    Upgrade tests to get rid of @testing-library

--- test/unit/web.test.tsx
+++ test/unit/web.test.tsx
@@ -2,14 +2,32 @@
  * @jest-environment jsdom
  */
 
+global.IS_REACT_ACT_ENVIRONMENT = true;
+
 import assert from 'assert';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { createRoot, Root } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
 
 import { View } from 'react-native-web';
 import { BoundaryProvider, useRef, useBoundary } from 'react-ref-boundary';
 
 describe('react-native-web', function () {
+  let container: HTMLDivElement | null = null;
+  let root: Root | null = null;
+  beforeEach(function () {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    root = createRoot(container);
+  });
+
+  afterEach(function () {
+    act(() => root.unmount());
+    root = null;
+    container.remove();
+    container = null;
+  });
+
   function NonBoundaryComponent() {
     const ref = React.useRef<View>(null);
     return <View ref={ref} />;
@@ -25,19 +43,21 @@ describe('react-native-web', function () {
     getRefs(boundary.refs);
     return <View />;
   }
-  it('refs', async function () {
+  it('refs', function () {
     let refs = [];
     function getRefs(x) {
       refs = x;
     }
     assert.equal(refs.length, 0);
-    render(
-      <BoundaryProvider>
-        <BoundaryComponent />
-        <NonBoundaryComponent />
-        <BoundaryComponent />
-        <BoundaryChecker getRefs={getRefs} />
-      </BoundaryProvider>,
+    act(() =>
+      root.render(
+        <BoundaryProvider>
+          <BoundaryComponent />
+          <NonBoundaryComponent />
+          <BoundaryComponent />
+          <BoundaryChecker getRefs={getRefs} />
+        </BoundaryProvider>,
+      ),
     );
     assert.equal(refs.length, 2);
   });
