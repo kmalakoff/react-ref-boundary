5f9975defe40cf8af8e16a2c901b97f6cd5b4e30
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:21:32 -0800

    Migrate to Biome and update test runner configuration

--- test/exports/require.test.cjs
+++ test/exports/require.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
 const { BoundaryProvider, useRef, useBoundary } = require('react-ref-boundary');
 
-describe('exports .cjs', function () {
-  it('defaults', function () {
+describe('exports .cjs', () => {
+  it('defaults', () => {
     assert.equal(typeof BoundaryProvider, 'function');
     assert.equal(typeof useRef, 'function');
     assert.equal(typeof useBoundary, 'function');
--- test/exports/unpkg.min.test.cjs
+++ test/exports/unpkg.min.test.cjs
@@ -1,13 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const {
-  BoundaryProvider,
-  useRef,
-  useBoundary,
-} = require('react-ref-boundary/dist/umd/react-ref-boundary.min.js');
+const { BoundaryProvider, useRef, useBoundary } = require('react-ref-boundary/dist/umd/react-ref-boundary.min.cjs');
 
-describe('exports react-ref-boundary/dist/umd/react-ref-boundary.min.js', function () {
-  it('defaults', function () {
+describe('exports react-ref-boundary/dist/umd/react-ref-boundary.min.cjs', () => {
+  it('defaults', () => {
     assert.equal(typeof BoundaryProvider, 'function');
     assert.equal(typeof useRef, 'function');
     assert.equal(typeof useBoundary, 'function');
