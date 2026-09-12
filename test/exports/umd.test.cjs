kmalakoff/react-native-contains

Update package exports and type definitions

test/exports/umd.test.cjs
@@ -1,8 +1,15 @@
 const assert = require('assert');
-const contains = require('react-native-contains/dist/umd/react-native-contains.cjs');
 
-describe('exports react-native-contains/dist/umd/react-native-contains.cjs', () => {
+let umd = null;
+try {
+  umd = require('react-native-contains/umd');
+} catch (_) {
+  umd = require('react-native-contains/dist/umd/react-native-contains.cjs');
+}
+const reactNativeContains = typeof window !== 'undefined' ? window.reactNativeContains : umd.default || umd;
+
+describe('exports umd', () => {
   it('defaults', () => {
-    assert.equal(typeof contains, 'function');
+    assert.equal(typeof reactNativeContains, 'function');
   });
 });

kmalakoff/react-ref-boundary

test/exports/umd.test.cjs
@@ -1,7 +1,15 @@
 const assert = require('assert');
-const { BoundaryProvider, useRef, useBoundary } = require('react-ref-boundary/dist/umd/react-ref-boundary.cjs');
 
-describe('exports react-ref-boundary/dist/umd/react-ref-boundary.cjs', () => {
+let umd = null;
+try {
+  umd = require('react-ref-boundary/umd');
+} catch (_) {
+  umd = require('react-ref-boundary/dist/umd/react-ref-boundary.cjs');
+}
+const reactRefBoundary = typeof window !== 'undefined' ? window.reactRefBoundary : umd.default || umd;
+const { BoundaryProvider, useRef, useBoundary } = reactRefBoundary;
+
+describe('exports umd', () => {
   it('defaults', () => {
     assert.equal(typeof BoundaryProvider, 'function');
     assert.equal(typeof useRef, 'function');